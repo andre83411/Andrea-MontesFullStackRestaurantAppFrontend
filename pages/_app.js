d9f4230ecb4ded509b889b4b267c6f5a19718228
andre83411 <andreamontes.rei@gmail.com>
2023-07-22 03:40:43 -0600

    updated all url to new strapi url on heroku

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { AppProvider } from "@/context/AppContext";
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
 
-const API_URL = process.env.STRAPI_URL || "http://localhost:1337";
+const API_URL = process.env.STRAPI_URL || "https://restaurantapp-backend-1a6af343f420.herokuapp.com";
 
 export const client = new ApolloClient({
   uri: `${API_URL}/graphql`,
